2b529462d433c1161265c93141a6416ec692bee4
Neykoor💜 <chinquepapa@gmail.com>
2025-07-06 21:36:06 -0700

    Update keyhelper.ts

--- src/Signal/Group/keyhelper.ts
+++ src/Signal/Group/keyhelper.ts
@@ -1,14 +1,21 @@
 import * as nodeCrypto from 'crypto'
 import { generateKeyPair } from 'libsignal/src/curve'
+import MAIN_LOGGER from '../../Utils/logger' // Importar el logger
+
+const logger = MAIN_LOGGER.child({}) // Inicializar el logger
 
 type KeyPairType = ReturnType<typeof generateKeyPair>
 
 export function generateSenderKey(): Buffer {
-	return nodeCrypto.randomBytes(32)
+	const senderKey = nodeCrypto.randomBytes(32)
+	logger.debug('Generated new sender key.') //
+	return senderKey
 }
 
 export function generateSenderKeyId(): number {
-	return nodeCrypto.randomInt(2147483647)
+	const senderKeyId = nodeCrypto.randomInt(2147483647)
+	logger.debug(`Generated new sender key ID: ${senderKeyId}.`) //
+	return senderKeyId
 }
 
 export interface SigningKeyPair {
@@ -17,12 +24,19 @@ export interface SigningKeyPair {
 }
 
 export function generateSenderSigningKey(key?: KeyPairType): SigningKeyPair {
+	let actualKeyPair: KeyPairType
 	if(!key) {
-		key = generateKeyPair()
+		actualKeyPair = generateKeyPair()
+		logger.debug('Generated new sender signing key pair.') //
+	} else {
+		actualKeyPair = key
+		logger.debug('Using provided key pair to generate sender signing key.') //
 	}
 
-	return {
-		public: Buffer.from(key.pubKey),
-		private: Buffer.from(key.privKey)
+	const signingKeyPair = {
+		public: Buffer.from(actualKeyPair.pubKey),
+		private: Buffer.from(actualKeyPair.privKey)
 	}
+	logger.debug('Formatted sender signing key pair.') //
+	return signingKeyPair
 }
